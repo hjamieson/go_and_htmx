438abc4f6f49225e8677c41927eb281373a4c59c
Hugh Jamieson <hjamieson5@gmail.com>
2024-12-01 13:03:44 -0500

    server1 does auto redirect

--- server1/server.go
+++ server1/server.go
@@ -11,7 +11,9 @@ func main() {
 		w.Write([]byte(`<h1>Clicked!</h1>`))
 	})
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/static/quick.html", http.StatusFound)
+	})
 	slog.Info("Server", "port", 3000)
 	http.ListenAndServe(":3000", nil)
 }
